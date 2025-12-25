f42638c56117798135a746de7b1c399734708e4d
cdmsales10-crypto <Btechruchulu09@gmail.com>
2025-12-25 06:07:29 +0530

    commit-4

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -125,7 +125,7 @@ export default function HomePage() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
             {products.map((product) => (
               <div
                 key={product.id}
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -93,7 +93,7 @@ export default function ProductsPage() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
           {filteredProducts.map((product) => (
             <div
               key={product.id}
